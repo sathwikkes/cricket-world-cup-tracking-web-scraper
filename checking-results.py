c936097b7a45de72d610c126829bc9f144d2f426
Sathwik Kesappragada <sathwik.kesappragada97@gmail.com>
2023-10-25 15:26:47 -0700

    Refactored code, added chron job so scraper runs once every day, results get immediately stored to database

--- checking-results.py
+++ checking-results.py
@@ -1,22 +1,54 @@
+# Import the Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from saving_data import Player, Team  # Import your database models
 
 # Create a database connection
 engine = create_engine('sqlite:///cricket.db')
+# Declare a Base using `automap_base()`
+Base = automap_base()
+# Use the Base class to reflect the database tables
+Base.prepare(autoload_with=engine)
+# Print all of the classes mapped to the Base
+Base.classes.keys()
+players = Base.classes.players
+teams = Base.classes.teams
+matchesplayed = Base.classes.MatchesPlayed
 
 # Create a Session
-Session = sessionmaker(bind=engine)
-session = Session()
-
-players = session.query(Player).all()
-teams = session.query(Team).all()
+#Session = sessionmaker(bind=engine)
+session = Session(engine)
 
 
-print("Players:")
-for player in players:
-    print(f"{player.stat_name} {player.first_name} {player.last_name} {player.rating} {player.nationality}")
+players_data = session.query(players).all()
 
-print("\nTeams:")
-for team in teams:
-    print(f"{team.stat_name} {team.rating}  {team.country}")
+# Print all rows in the Players table
+for player in players_data:
+    print(player.id, player.stat_name, player.first_name, player.last_name, player.nationality, player.rating)
+
+# Query the Teams table
+teams_data = session.query(teams).all()
+
+# Print all rows in the Teams table
+for team in teams_data:
+    print(team.id, team.stat_name, team.rating, team.country)
+
+# Query the MatchesPlayed table
+matches_played_data = session.query(matchesplayed).all()
+
+# Print all rows in the MatchesPlayed table
+for match in matches_played_data:
+    print(match.id, match.date, match.matches_played)
+
+# players = session.query(Player).all()
+# teams = session.query(Team).all()
+
+
+# print("Players:")
+# for player in players:
+#     print(f"{player.stat_name} {player.first_name} {player.last_name} {player.rating} {player.nationality}")
+
+# print("\nTeams:")
+# for team in teams:
+#     print(f"{team.stat_name} {team.rating}  {team.country}")
--- main.py
+++ main.py
@@ -1,150 +1,182 @@
-# Importing Libraries
+# Import the necessary libraries
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-# Options
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
+import datetime
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.orm import declarative_base, sessionmaker
+Base = declarative_base()
 
-wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
+# Define your database classes (MatchesPlayed, Player, Team)
+# ...
+# Creates Player, Matches Played, and Team Classes which will serve as the anchor points for our Tables
+class MatchesPlayed(Base):
+    __tablename__ = 'MatchesPlayed'
 
-wd.get("https://www.cricketworldcup.com/tournament-stats")
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    matches_played = Column(Integer)
 
-#parent_elements = wd.find_elements(By.CLASS_NAME,"tournament-stats__row tournament-stats__row--player-stats")
+class Player(Base):
+    __tablename__ = 'players'
 
-player_stat_name = []
-player_stat = []
-player_fname = []
-player_lname =[]
-player_country= []
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    stat_name = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    nationality = Column(String)
+    rating = Column(String)
 
-team_stat_name = []
-team_stat = []
-country = []
+class Team(Base):
+    __tablename__ = 'teams'
 
-# Locate the parent element by class name
-parent_element = wd.find_element(By.CLASS_NAME, "tournament-stats__row--player-stats")
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    stat_name = Column(String)
+    rating = Column(String)
+    country = Column(String)
 
-# Find and extract all h4 elements (stat titles)
-stat_titles = parent_element.find_elements(By.CLASS_NAME,"tournament-stats-card__title")
+# Function to scrape data from the website
+def scrape_data():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
 
-# Find and extract all first name elements
-first_names = parent_element.find_elements(By.CLASS_NAME,"tournament-stats-card__first-name")
+    wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-# Find and extract all last name elements
-last_names = parent_element.find_elements(By.CLASS_NAME,"tournament-stats-card__last-name")
+    wd.get("https://www.cricketworldcup.com/tournament-stats")
 
-player_ratings = parent_element.find_elements(By.CLASS_NAME,"tournament-stats-card__rating")
+    player_stat_name = []
+    player_stat = []
+    player_fname = []
+    player_lname =[]
+    player_country= []
 
-# Find and extract all nationality elements
-nationalities = parent_element.find_elements(By.CLASS_NAME,"tournament-stats-card__nation--abbr")
+    team_stat_name = []
+    team_stat = []
+    country = []
 
-# Iterate through the elements and extract their text
-for i in range(len(stat_titles)):
-    #print("Stat Title:", stat_titles[i].text)
-    player_stat_name.append(stat_titles[i].text)
-    #print("First Name:", first_names[i].text)
-    player_fname.append(first_names[i].text)
-    #print("Last Name:", last_names[i].text)
-    player_lname.append(last_names[i].text)
-    #print("AMT", player_ratings[i].text)
-    player_stat.append(player_ratings[i].text)
-    #print("Nationality:", nationalities[i].text)
-    player_country.append(nationalities[i].text)
-    #print("\n")
+    # Locate the parent element by class name
+    parent_element = wd.find_element(By.CLASS_NAME, "tournament-stats__row--player-stats")
 
+    # Find and extract all h4 elements (stat titles)
+    stat_titles = parent_element.find_elements(By.CLASS_NAME,"tournament-stats-card__title")
 
-# Locate the parent element by class name
-parent_elements = wd.find_element(By.CLASS_NAME,"tournament-stats__row--team-stats")
+    # Find and extract all first name elements
+    first_names = parent_element.find_elements(By.CLASS_NAME,"tournament-stats-card__first-name")
 
-# Locate the "Matches Played" value using its CSS selector
-matches_played_element = wd.find_element(By.CSS_SELECTOR,".wheel-chart.js-matches-played .wheel-chart__value.js-value-counter")
+    # Find and extract all last name elements
+    last_names = parent_element.find_elements(By.CLASS_NAME,"tournament-stats-card__last-name")
 
-if matches_played_element:
-    # Extract and print the "Matches Played" value
-    matches_played = matches_played_element.get_attribute("data-value")
-    print("Matches Played:", matches_played)
-else:
-    print("Matches Played element not found.")
+    player_ratings = parent_element.find_elements(By.CLASS_NAME,"tournament-stats-card__rating")
 
-# Find and extract card titles
-card_titles = parent_elements.find_elements(By.CLASS_NAME,"tournament-stats-card__title")
+    # Find and extract all nationality elements
+    nationalities = parent_element.find_elements(By.CLASS_NAME,"tournament-stats-card__nation--abbr")
 
-# Find and extract card ratings
-card_ratings = parent_elements.find_elements(By.CLASS_NAME,"tournament-stats-card__rating")
-
-# Find and extract card nationalities
-card_nationalities = parent_elements.find_elements(By.CLASS_NAME,"tournament-stats-card__nation--abbr")
-
-# Find and extract card nationalities for away team
-away_team_elements = parent_elements.find_elements(By.CLASS_NAME, "tournament-stats-card__nation.tournament-stats-card__nation--away-team.tournament-stats-card__nation--abbr")
-
-
-if card_nationalities and away_team_elements:
-    for i in range(len(card_titles)):
-        #print("Card Title:", card_titles[i].text)
-        #print("Card Rating:", card_ratings[i].text)
-        team_stat_name.append(card_titles[i].text)
-        team_stat.append(card_ratings[i].text)
-
-        if i < len(card_nationalities):
-            card_nationality = card_nationalities[i].text
-
-            # Check if there's an "away team nationality" available
-            if i == (len(card_titles)-1):
-                for i in range(len(away_team_elements)):
-                    away_team_nationality = away_team_elements[i].text
-                card_nationality += " vs " + away_team_nationality
-            country.append(card_nationality)
-            #print("Card Nationality:", card_nationality)
-        print("\n")
-else:
-    print("Card and Away Team Nationalities not available.")
+    # Iterate through the elements and extract their text
+    for i in range(len(stat_titles)):
+        player_stat_name.append(stat_titles[i].text)
+        player_fname.append(first_names[i].text)
+        player_lname.append(last_names[i].text)
+        player_stat.append(player_ratings[i].text)
+        player_country.append(nationalities[i].text)
+        #print("\n")
 
 
+    # Locate the parent element by class name
+    parent_elements = wd.find_element(By.CLASS_NAME,"tournament-stats__row--team-stats")
 
-print(player_stat_name)
-print(player_fname)
-print(player_lname)
-print(player_stat)
-print(player_country)
-print("\n")
-print("------------")
-print(team_stat_name)
-print(team_stat)
-print(country)
+    # Locate the "Matches Played" value using its CSS selector
+    matches_played_element = wd.find_element(By.CSS_SELECTOR,".wheel-chart.js-matches-played .wheel-chart__value.js-value-counter")
+
+    if matches_played_element:
+        # Extract and print the "Matches Played" value
+        matches_played = matches_played_element.get_attribute("data-value")
+        print("Matches Played:", matches_played)
+    else:
+        print("Matches Played element not found.")
+
+    # Find and extract card titles
+    card_titles = parent_elements.find_elements(By.CLASS_NAME,"tournament-stats-card__title")
+
+    # Find and extract card ratings
+    card_ratings = parent_elements.find_elements(By.CLASS_NAME,"tournament-stats-card__rating")
+
+    # Find and extract card nationalities
+    card_nationalities = parent_elements.find_elements(By.CLASS_NAME,"tournament-stats-card__nation--abbr")
+
+    # Find and extract card nationalities for away team
+    away_team_elements = parent_elements.find_elements(By.CLASS_NAME, "tournament-stats-card__nation.tournament-stats-card__nation--away-team.tournament-stats-card__nation--abbr")
 
 
-# for i in range(len(card_titles)):
-#     print("Card Title:", card_titles[i].text)
-#     print("Card Rating:", card_ratings[i].text)
-#     print("Card Nationality:", card_nationalities[i].text)
-#     print("\n")
+    if card_nationalities and away_team_elements:
+        for i in range(len(card_titles)):
+            team_stat_name.append(card_titles[i].text)
+            team_stat.append(card_ratings[i].text)
+            if i < len(card_nationalities):
+                card_nationality = card_nationalities[i].text
+                # Check if there's an "away team nationality" available
+                if i == (len(card_titles)-1):
+                    for i in range(len(away_team_elements)):
+                        away_team_nationality = away_team_elements[i].text
+                    card_nationality += " vs " + away_team_nationality
+                country.append(card_nationality)
+            print("\n")
+    else:
+        print("Card and Away Team Nationalities not available.")
 
+    return player_stat_name, player_stat, player_fname, player_lname, player_country, team_stat, team_stat_name, country, matches_played
 
+# Function to connect to the database
+def connect_to_database():
+    # Create the database engine and tables
+    engine = create_engine('sqlite:///cricket.db')
+    conn = engine.connect()
+    Base.metadata.create_all(conn)
 
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# for i in range(len(away_team_elements)):
-#     print("Away Team Nationality:", away_team_elements[i].text)
-#     print("\n")
+    return session
 
- 
+# Function to add data to the database
+def add_data_to_database(session, statistic, player_rating, player_first_name, player_last_name, player_nationality, team_stat_name, team_data, team_country, matches_played):
+    player_stat_name, player_stat, player_fname, player_lname, player_country, team_stat, team_stat_name, country =  statistic, player_rating, player_first_name, player_last_name, player_nationality, team_stat_name, team_data, team_country
 
-# # Iterate through the parent elements
-# for parent_element in parent_elements:
-#     # Locate the child elements within the parent element
-#     stat_title = parent_element.find_element(By.CLASS_NAME, "tournament-stats-card__title")
-#     first_name = parent_element.find_element(By.CLASS_NAME,"tournament-stats-card__first-name")
-#     last_name = parent_element.find_element(By.CLASS_NAME,"tournament-stats-card__last-name")
-#     nationality = parent_element.find_element(By.CLASS_NAME,"tournament-stats-card__nation--abbr")
+    date = datetime.date.today()
 
-#     # Extract and print the text from each element
-#     print("Stat Title:", stat_title.text)
-#     print("First Name:", first_name.text)
-#     print("Last Name:", last_name.text)
-#     print("Nationality:", nationality.text)
-#     print("\n")
+    for i in range(len(player_stat_name)):
+        player = Player(
+            stat_name=player_stat_name[i],
+            first_name=player_fname[i],
+            last_name=player_lname[i],
+            nationality=player_country[i],
+            rating=player_stat[i]
+        )
+        session.add(player)
+
+    for i in range(len(team_stat_name)):
+        team = Team(
+            stat_name=team_stat_name[i],
+            rating=team_stat[i],
+            country=country[i]
+        )
+        session.add(team)
+
+    new_match = MatchesPlayed(date=date, matches_played=matches_played)
+    session.add(new_match)
+
+    session.commit()
+    session.close()
+
+    print("Saved data...")
+
+def main():
+    statistic, player_rating, player_first_name, player_last_name, player_nationality, team_stat_name, team_data, team_country, matches_played = scrape_data()
+    session = connect_to_database()
+    add_data_to_database(session,  statistic, player_rating, player_first_name, player_last_name, player_nationality, team_stat_name, team_data, team_country, matches_played)
+
+if __name__ == "__main__":
+    main()
