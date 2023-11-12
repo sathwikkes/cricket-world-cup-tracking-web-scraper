fa12fe9f2bba0e824ad21039fa1d2314bbdb92f5
Sathwik Kesappragada <sathwik.kesappragada97@gmail.com>
2023-11-11 22:06:08 -0800

    added in a couple more routes, transformed data into json, calculated who held the most wickets the longest in js

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
@@ -22,11 +22,16 @@ matchesplayed = Base.classes.MatchesPlayed
 #engine = create_engine('sqlite:///cricket.db')
 #Base.metadata.bind = engine
 
-session = Session(bind=engine)
 
 
 @app.route('/')
 def display_data():
+    #good practice to close the database session after using it
+    """
+    losing the session is to ensure that resources associated with the database (such as database connections) are properly managed.
+    The Session in SQLAlchemy represents an ongoing conversation between the application and the database, and it's important to close the session when you're done using it to release resources and prevent potential issues with connection pooling.
+    """
+    
     # Query the Players table
     # players_data = session.query(players).all()
 
@@ -37,6 +42,7 @@ def display_data():
     # matches_played_data = session.query(matchesplayed).all()
 
     # return render_template('index.html', players=players_data, teams=teams_data, matches_played=matches_played_data)
+    session = Session(bind=engine)
     highest_score_players = session.query(players).filter(players.stat_name == "HIGHEST SCORE").all()
 
     most_runs_players = session.query(players).filter(players.stat_name == "MOST RUNS").all()
@@ -48,11 +54,87 @@ def display_data():
     highest_innings_score_teams = session.query(teams).filter(teams.stat_name == "HIGHEST INNINGS SCORE").all()
 
     best_win_percentage_teams = session.query(teams).filter(teams.stat_name == "BEST WIN PERCENTAGE").all()
-
+    session.close()
     return render_template('index.html', highest_score_players=highest_score_players,
                            most_runs_players=most_runs_players, most_wickets_players=most_wickets_players,
                            highest_innings_score_teams=highest_innings_score_teams,
                            best_win_percentage_teams=best_win_percentage_teams)
 
+
+@app.route('/dictdata')
+def data_transform():
+    
+    session = Session(bind=engine)
+
+    matches_played_data = session.query(matchesplayed).all()
+    players_data = session.query(players).all()
+
+    combined_data = []
+
+
+    for match in matches_played_data:
+        player_ids_for_match = [match.id, match.id + 1, match.id + 2]
+        players_for_match = [player for player in players_data if player.id in player_ids_for_match]
+        combined_data.append({'match_data': match, 'players_data': players_for_match})
+
+        # Initialize a variable to keep track of the player ID
+    player_id = 1
+
+    # Initialize an empty dictionary to store the final results
+    final_data = {}
+
+    # Iterate through combined_data
+    for row in combined_data:
+        # Access the 'match_data' from each row
+        match_data = row['match_data']
+        
+        # Extract the 'Match ID' from match_data
+        match_id = match_data.id
+        matches_played = match_data.matches_played
+        match_date = match_data.date
+
+        # Create a list of 'Player IDs' for the current 'Match ID'
+        player_ids = list(range(player_id, player_id + 3))
+
+        player_query = session.query(players.first_name, players.last_name, players.stat_name, players.nationality, players.rating).filter(players.id.in_(player_ids))
+
+        # Initialize an empty list to store player data
+        player_data = []
+
+        # Iterate through the player names query result
+        for player in player_query:
+            player_name = f"{player.first_name} {player.last_name}"
+            stat_name = player.stat_name
+            nationality = player.nationality
+            rating = player.rating
+
+            # Create a dictionary for the current player
+            player_dict = {
+                'Player Name': player_name,
+                'Stat Name': stat_name,
+                'Nationality': nationality,
+                'Rating': rating
+            }
+            # Append the player dictionary to the player_data list
+            player_data.append(player_dict)
+
+        # Update the player_id counter
+        player_id += 3
+
+        if match_id in final_data:
+            final_data[match_id]['Player Data'].update(player_data)
+        else:
+            final_data[match_id] = {'Match ID': match_id, "Matches Played":matches_played , "Date Scraped":match_date, 'Player Data': player_data}
+    session.close()
+    # print(final_data)
+    return(jsonify(final_data))
+
+@app.route('/analysis')
+def analysis():
+    return render_template('result.html')
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8000,debug=True)
+
---
+++ static/script.js
@@ -0,0 +1,58 @@
+// Function to parse date strings into Date objects
+function parseDate(dateString) {
+  const parsedDate = new Date(dateString);
+  return isNaN(parsedDate.getTime()) ? null : parsedDate;
+}
+
+// Wait for the DOM to be ready
+document.addEventListener('DOMContentLoaded', function () {
+  // Make a request to your Flask route
+  fetch('/dictdata')
+    .then(response => response.json())
+    .then(data => {
+      // Get the textarea element
+      var textarea = document.getElementById('data');
+
+      // Display the JSON data in the textarea
+      textarea.value = JSON.stringify(data, null, 2);
+      
+      let longestStreakPlayer = "";
+      let longestStreak = 0;
+      let longestStreakDays = 0;
+      let currentStreakPlayer = "";
+      let currentStreak = 0;
+      let currentStreakStart = "";
+      
+      for (let match in data) {
+        let players = data[match]["Player Data"];
+        for (let player of players) {
+          if (player["Stat Name"] === "MOST WICKETS") {
+            if (player["Player Name"] === currentStreakPlayer) {
+              currentStreak += 1;
+            } else {
+              currentStreakPlayer = player["Player Name"];
+              currentStreak = 1;
+              currentStreakStart = new Date(data[match]["Date Scraped"]);
+            }
+            if (currentStreak > longestStreak) {
+              longestStreak = currentStreak;
+              longestStreakPlayer = currentStreakPlayer;
+              let startDate = new Date(currentStreakStart.getFullYear(), currentStreakStart.getMonth(), currentStreakStart.getDate());
+              let endDate = new Date(data[match]["Date Scraped"]);
+              endDate = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
+              longestStreakDays =Math.floor( (endDate - startDate) / (1000 * 60 * 60 * 24) + 1);
+            }
+          }
+        }
+      }
+      
+      //console.log(longestStreakPlayer, longestStreakDays);
+      // Display the result on the page
+      var resultDiv = document.getElementById('result');
+      resultDiv.innerHTML = `
+        <p>Player with MOST WICKETS held the stat the longest: ${longestStreakPlayer}</p>
+        <p>Days held: ${longestStreakDays}</p>
+      `;
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
